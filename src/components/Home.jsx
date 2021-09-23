4ee09671b6604a05f076e1dddefa39610a64ce8a
gouder hicham <gouderhicham@gmail.com>
2021-09-23 17:59:15 +0100

    fixed the nav bug and changed the logo

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import background from "../images/background.jpeg";
+export default function Home() {
+  return (
+    <>
+      <div
+        className="bk-img"
+      ></div>
+    </>
+  );
+}
