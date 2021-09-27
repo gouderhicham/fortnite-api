9a7c95c6e980ee8787f4824549c8dbd0b07a0560
gouder hicham <gouderhicham@gmail.com>
2021-09-27 11:47:00 +0100

    completed the grid boxes in the main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,7 +4,7 @@ import {ItemsData} from "../data/items"
 export default function MainPage() {
     return (
         <div className = "grid-container">
-            {ItemsData.map((grid, i) => <Grid i = {i} title = {grid.title} img = {grid.img} date = {grid.date} className = {`grid-colomn_${i}`} /> )}
+            {ItemsData.map((grid, i) => <Grid i = {i} title = {grid.title} img = {grid.img} date = {grid.date} className = {`grid-colomn_${i +1 }`} /> )}
         </div>
     )
 }
