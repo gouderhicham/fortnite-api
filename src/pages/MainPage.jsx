b1662a4753fe023767ad6a5bbb5a065720377933
gouder hicham <gouderhicham@gmail.com>
2021-09-28 18:37:55 +0100

    see you after you get 17

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import Grid from "../components/Grid"
-import {ItemsData} from "../data/items"
+import React from "react";
+import Grid from "../components/Grid";
+import { ItemsData } from "../data/items";
 export default function MainPage() {
-    return (
-        <div className = "grid-container">
-            {ItemsData.map((grid, i) => <Grid i = {i} title = {grid.title} img = {grid.img} date = {grid.date} className = {`grid-colomn_${i +1 }`} /> )}
-        </div>
-    )
+  return (
+    <>
+      <div className="main-page_bk-img">
+          <p>fortnite</p>
+      </div>
+      <div className="grid-container">
+        {ItemsData.map((grid, i) => (
+          <Grid
+            i={i}
+            title={grid.title}
+            img={grid.img}
+            date={grid.date}
+            className={`grid-colomn_${i + 1}`}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
